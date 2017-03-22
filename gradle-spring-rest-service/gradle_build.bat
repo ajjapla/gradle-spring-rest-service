15ef5ea7edde7ed73c3347cdb64d0e3ddea1e408
aajjapla_c <anildvg2@gmail.com>
2017-03-22 12:03:49 -0700

    initial commit

---
+++ gradle-spring-rest-service/gradle_build.bat
@@ -0,0 +1,19 @@
+title gradle build
+SET JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_121
+SET PATH=C:\Anil\Gradle\gradle-3.4.1\bin;C:\Program Files (x86)\Java\jdk1.8.0_121\bin;.;%PATH%;
+
+REM gradle build --stacktrace
+REM gradle wrapper --gradle-version 3.4.1
+
+REM gradle clean
+
+REM gradle eclipse
+
+REM gradlew build --stacktrace
+
+REM java -jar build\libs\gs-rest-service-0.1.0.jar
+
+gradlew bootRun
+
+
+pause
